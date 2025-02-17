ae29ff3b2655f72a7f4434db10d2cf3203b4b5a8
Alex Jeffcott <9036846+AlexJeffcott@users.noreply.github.com>
2025-02-17 12:56:13 +0100

    remove spa functinality

--- src/actions-ui/mod.ts
+++ src/actions-ui/mod.ts
@@ -1,4 +1,2 @@
 export * from './toggle-theme-btn/mod.tsx'
 export * from './toggle-color-theme-btn/mod.tsx'
-export * from './navigate-to-code-btn/mod.tsx'
-export * from './navigate-to-home-btn/mod.tsx'
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { render } from 'preact'
 import { StoresProvider } from '@/contexts/stores.tsx'
 import { initActionListeners } from '@/actions/init-action-listeners.ts'
 import { createStores } from '@/stores/mod.ts'
-import { CodePage, HomePage } from '@/pages/mod.ts'
+import { CodePage as Page } from '@/pages/code/mod.tsx'
 
 import '@/global.css'
 
@@ -12,19 +12,9 @@ if (element instanceof HTMLElement) {
   const stores = createStores()
   initActionListeners(element, stores)
 
-  const Router = () => {
-    const path = stores.routerStore.currentPath.value
-    switch (path) {
-      case '/code':
-        return <CodePage />
-      default:
-        return <HomePage />
-    }
-  }
-
   render(
     <StoresProvider stores={stores}>
-      <Router />
+      <Page />
     </StoresProvider>,
     element,
   )
